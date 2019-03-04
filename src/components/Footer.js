c0f5df5163fc411f3970db7985699bf393fcc27f
Sam Zhou <sxuzhou@outlook.com>
2019-03-03 21:55:38 -0500

    Added the about page.
    Bootstrap working.
    
    Preliminary done.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,8 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/sam-zhou/" target="_blank" rel="noopener noreferrer" className="icon fa-linkedin"><span className="label">Linkdin</span></a></li>
+                        <li><a href="https://github.com/sxuzhou" target="_blank" rel="noopener noreferrer" className="icon fa-github"><span className="label">Github</span></a></li>
                     </ul>
                     <ul className="copyright">
                         <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -87,11 +87,9 @@ class HomeIndex extends React.Component {
                         </header>
 
 
-                        <p>
-                            Where would you like to start?
-                        </p>
+
                         <ul className="actions">
-                            <li><a href="#two" className="button">Learn More</a></li>
+                            <li><a href="#two" className="button">Get Started!</a></li>
                         </ul>
                     </section>
 
@@ -147,46 +145,49 @@ class HomeIndex extends React.Component {
 
 
                         <ul className="actions">
-                            <li><a href="#three" className="button">Full Portfolio</a></li>
+                            <li><a href="#about" className="button">Contact Me To Learn More!</a></li>
                         </ul>
                     </section>
 
                     <section id="three">
-                        <h2>Get In Touch</h2>
-                        <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
+                        <h2>How I Like My Code</h2>
+                        <p>Here are some of the philosophies I try to implement when coding in a production environment</p>
+                        <ul>
+                            <li>
+                                Keep the code D.R.Y(Don't Repeat Yourself).
+                            </li>
+                            <li>
+                                Keep the code simple (KISS) and optimize at the end of development.
+                            </li>
+                            <li>
+                                Break big issue into small but self contained and readable commits. (For code reviewer)
+                            </li>
+                        </ul>
+                        <p>I'm super open to changing my practices based on feedback, culture and language and always looking for ways improve.</p>
+                    </section>
+                    <section id="about">
+                        <h2>About</h2>
+                        <p className="paragraph__margin"><i>For my Linkdin and Github, check out the side bar footer.</i></p>
+                        <i>This site was created using the <a href="https://github.com/codebushi/gatsby-starter-strata" target="_blank" rel="noopener noreferrer">GatsbyJs starter pack: Strata by codebushi</a>
+                        </i>
+
+
                         <div className="row">
-                            <div className="8u 12u$(small)">
-                                <form method="post" action="#">
-                                    <div className="row uniform 50%">
-                                        <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
-                                        <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
-                                        <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"/></div>
-                                    </div>
-                                </form>
-                                <ul className="actions">
-                                    <li><input type="submit" value="Send Message" /></li>
-                                </ul>
-                            </div>
+                            <p>
+                                For resume and inquires you can contact me @
+                            </p>
                             <div className="4u 12u$(small)">
                                 <ul className="labeled-icons">
                                     <li>
-                                        <h3 className="icon fa-home"><span className="label">Address</span></h3>
-                                        1234 Somewhere Rd.<br />
-                                        Nashville, TN 00000<br />
-                                        United States
-                                    </li>
-                                    <li>
-                                        <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
-                                        000-000-0000
-                                    </li>
-                                    <li>
                                         <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
-                                        <a href="#">hello@untitled.tld</a>
+                                        <a >samzhoucontact@gmail.com</a>
                                     </li>
                                 </ul>
                             </div>
+
                         </div>
                     </section>
+
 
                 </div>
 
